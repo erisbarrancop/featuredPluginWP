2f10a8bdbb2d1002b30d9fb3f67508e9aeef298c
erisbarrancop <ibarpri1712@iesfuengirola1.es>
2024-11-18 13:46:48 +0100

    Set up post metadata that will allow us to create a relationship between a blog post and it´s featured professors

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data'), 'version' => '9002a2896c4eeb903480');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data'), 'version' => '5c8a741de48392803fda');
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./index.scss"
-import {useSelect} from "@wordpress/data"
-import {useState, useEffect} from "react"
+import { useSelect } from "@wordpress/data"
+import { useState, useEffect } from "react"
 import apiFetch from "@wordpress/api-fetch"
 
 wp.blocks.registerBlockType("ourplugin/featured-professor", {
@@ -9,7 +9,7 @@ wp.blocks.registerBlockType("ourplugin/featured-professor", {
   icon: "welcome-learn-more",
   category: "common",
   attributes: {
-    profId: {type:" string"}
+    profId: { type: " string" }
   },
   edit: EditComponent,
   save: function () {
@@ -20,40 +20,61 @@ wp.blocks.registerBlockType("ourplugin/featured-professor", {
 function EditComponent(props) {
   const [thePreview, setThePreview] = useState("")
 
-  useEffect(()=>{
-    async function go() {
-      const response = await apiFetch({
-        path: `/featuredProfessor/v1/getHTML?profId=${props.attributes.profId}`,
-        method: "GET"
-      })
-      setThePreview(response)
+  useEffect(() => {
+    if (props.attributes.profId) {
+      updateTheMeta()
+      async function go() {
+        const response = await apiFetch({
+          path: `/featuredProfessor/v1/getHTML?profId=${props.attributes.profId}`,
+          method: "GET"
+        })
+        setThePreview(response)
+      }
+      go()
     }
-    go()
   }, [props.attributes.profId])
 
+  useEffect(() => {
+    return () => {
+      updateTheMeta()
+    }
+  }, [])
+
+  function updateTheMeta() {
+    const profsForMeta = wp.data.select("core/block-editor")
+      .getBlocks()
+      .filter(x => x.name == "ourplugin/featured-professor")
+      .map(x => x.attributes.profId)
+      .filter((x, index, arr) => {
+        return arr.indexOf(x) == index
+      })
+    console.log(profsForMeta)
+    wp.data.dispatch("core/editor").editPost({ meta: { featuredprofessor: profsForMeta } })
+  }
+
   const allProfs = useSelect(select => {
-    return select("core").getEntityRecords("postType", "professor", {per_page: -1})
+    return select("core").getEntityRecords("postType", "professor", { per_page: -1 })
   })
 
   console.log(allProfs)
 
-  if(allProfs == undefined) return <p>Loading...</p>
+  if (allProfs == undefined) return <p>Loading...</p>
 
   return (
     <div className="featured-professor-wrapper">
       <div className="professor-select-container">
-        <select onChange={e => props.setAttributes({profId: e.target.value})}>
+        <select onChange={e => props.setAttributes({ profId: e.target.value })}>
           <option value="">Select a professor</option>
           {allProfs.map(prof => {
             return (
-            <option value={prof.id} selected={props.attributes.profId == prof.id}>
-              {prof.title.rendered}
-            </option>
-          )
+              <option value={prof.id} selected={props.attributes.profId == prof.id}>
+                {prof.title.rendered}
+              </option>
+            )
           })}
         </select>
       </div>
-      <div dangerouslySetInnerHTML={{__html: thePreview}}></div>
+      <div dangerouslySetInnerHTML={{ __html: thePreview }}></div>
     </div>
   )
 }
