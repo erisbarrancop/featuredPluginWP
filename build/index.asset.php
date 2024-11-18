d56d6c93fad57dd4d09aa637e5d00b386ca5ad4f
erisbarrancop <ibarpri1712@iesfuengirola1.es>
2024-11-18 11:41:49 +0100

    Now the list is populated dynamically, and will display the different professor names in the dropdown list

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => 'b79caf36324ce8760c7c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-data'), 'version' => 'e65a44a1b1ab2472774b');
